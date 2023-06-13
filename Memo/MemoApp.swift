d2e9eaeb0107afdc436c8f5301c6a5289286d0a2
SuminJN <wjstnals1211@gmail.com>
2023-06-13 22:23:00 +0900

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 전수민 on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
